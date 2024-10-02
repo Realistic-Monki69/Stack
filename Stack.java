8ff8bef1b0face2c50c3a9e955c910cea656e7fb
Hrithik Turaka <hturaka@cherrycreekschools.org>
2024-10-02 17:29:00 +0000

    Done

--- Stack.java
+++ Stack.java
@@ -1,1 +1,40 @@
+import java.util.ArrayList;
 
+//H - type of elements in the stack
+//H - String, Integer, etc.
+public class Stack<H>{
+    private ArrayList<H> stack;
+    //main methods: push,pop, peek, isEmpty, size
+
+    public Stack(){
+        stack = new ArrayList<H>();
+        // can also do this: stack = new ArrayList<>();
+    }
+
+    public void push (H item){
+        stack.add(item);
+    }
+    
+    public H pop(){
+        if(isEmpty()){
+            return null;
+        }
+        return stack.remove(stack.size() - 1);
+    }
+
+    public H peek(){
+        if(isEmpty()){
+            return null;
+        }
+        //starts at 0
+        return stack.get(stack.size() -1);
+    }
+
+    public boolean isEmpty(){
+        return stack.isEmpty();
+    }
+
+    public int size(){
+        return stack.size();
+    }
+}
